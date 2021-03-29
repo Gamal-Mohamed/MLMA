b5d629d7f66909ec9349e1f196a12620ea59f77d
Gamal-Mohamed <49412012+Gamal-Mohamed@users.noreply.github.com>
2021-03-29 23:53:03 +0200

    run kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="gamalmohamed/udaproimage"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udaproimage --image=$dockerpath:udaproimage --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment udaproimage --port=80 --target-port=80
+kubectl port-forward deployment/udaproimage 8000:80
