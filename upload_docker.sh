4aa043126af312eeb8786d4d9252c34a021bda67
Gamal-Mohamed <49412012+Gamal-Mohamed@users.noreply.github.com>
2021-03-29 23:13:53 +0200

    adding docker push

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="Gamal-Mohamed/MLMA"
 
-# Step 2:  
+
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag MLMA:latest $dockerpath:MLMA
+ocker login --username gamalmohamed
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:MLMA:latest
